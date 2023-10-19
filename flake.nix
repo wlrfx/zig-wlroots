807a08e6b6929c891f15b5daa472e3813de3a828
Shinyzenith <aakashsensharma@gmail.com>
2023-10-19 18:58:27 +0530

    chore(flake): Add scenefx.nix
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
               pixman
               stdenv
               wlroots
+	       (callPackage ./scenefx.nix { })
               wayland
               wayland-protocols
               pkg-config
